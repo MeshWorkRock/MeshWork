64fc1af828b35ec254195d1c648503353ff40889
Aman <asingh999222@gmail.com>
2022-12-13 23:17:29 +0530

    Connected with Firebase

--- app/src/main/java/com/example/meshworkapp/MainActivity.kt
+++ app/src/main/java/com/example/meshworkapp/MainActivity.kt
@@ -11,10 +11,20 @@ import com.example.meshworkapp.composables.TopBarComposable
 import com.example.meshworkapp.navigationgraphs.RootNavGraph
 import com.example.meshworkapp.screens.HomeScreen
 import com.example.meshworkapp.ui.theme.MeshWorkAppTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
+    lateinit var fbFirestore: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        fbFirestore = FirebaseFirestore.getInstance()
+        val city = hashMapOf(
+            "name" to "Los Angeles",
+            "state" to "CA",
+            "country" to "USA"
+        )
+        fbFirestore.collection("TestCOll").document("TestDoc").set(city)
         setContent {
             MeshWorkAppTheme {
 //                TopBarComposable()
